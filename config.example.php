06adc06d536e329417f76eaeff9dec8ac7e75f08
Evelyn Hung <jj.evelyn@gmail.com>
2013-06-03 01:03:34 +0800

    update config example

--- config.example.php
+++ config.example.php
@@ -2,17 +2,17 @@
 # marksite configurations
 
 # path to website contents
-@define( "MARKSITE_SRC_PATH", "src/" );
-@define( "MARKSITE_DST_PATH", "html/" );
-@define( "MARKSITE_BLOCKS_PATH", "blocks/" );
+@define( "MARKSITE_SRC_PATH", "../../src/" );
+@define( "MARKSITE_DST_PATH", "../tmp/" );
+@define( "MARKSITE_BLOCKS_PATH", "../../src/blocks/" );
 
 # appcache generation method: all, parsed, none
 @define( "MARKSITE_APPCACHE", "all" );
 
 # path to theme
-@define( "MARKSITE_THEME_PATH", "src/theme/" );
+@define( "MARKSITE_THEME_PATH", "../../theme/marksite/" );
 
 # website path
-@define( "MARKSITE_ABSOLUTE_PATH", "http://example.com/" );
+@define( "MARKSITE_ABSOLUTE_PATH", "/2013-beta/" );
 
 ?>
